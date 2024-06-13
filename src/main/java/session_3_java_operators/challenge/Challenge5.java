d81a59adfecd6e2dc18201a4d32a569856171245
stoianovicideiana <deiana.stoianovici@gmail.com>
2024-06-13 13:05:17 +0300

    Session 3 - Challenge 4 done

---
+++ src/main/java/session_3_java_operators/challenge/Challenge5.java
@@ -0,0 +1,10 @@
+package session_3_java_operators.challenge;
+
+//5. Create a Java program that demonstrates the use of each compound assignment operator with integers.
+
+public class Challenge5 {
+
+    public static void main(String[] args) {
+
+    }
+}
