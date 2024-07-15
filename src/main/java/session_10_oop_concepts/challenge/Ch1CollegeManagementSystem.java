c54c1e896466337ff36c792020a100ca51b4035c
stoianovicideiana <deiana.stoianovici@gmail.com>
2024-07-15 18:22:58 +0300

    Session 10 - CH1 not finished - updates

--- src/main/java/session_10_oop_concepts/challenge/Ch1CollegeManagementSystem.java
+++ src/main/java/session_10_oop_concepts/challenge/Ch1CollegeManagementSystem.java
@@ -1,13 +1,30 @@
 package session_10_oop_concepts.challenge;
 
-//College Management System -  Design a system for a college to efficiently manage information about students, professors, and courses:
-//Store comprehensive profiles for every student, capturing their first name, last name, sex, age, date of birth, CNP, and address.
-//Similarly, maintain detailed profiles for every professor, including their first name, last name, sex, age, date of birth, CNP, and address.
-//The college offers eight distinct courses: Math, Computer Science, Physics, Chemistry, Biology, Geography, English, and History.
-// For each course, track details like its schedule, duration, description, and the assigned professor.
-//Allow for the assignment of a professor to a specific course and ensure the ability to retrieve and view course details, including its assigned professor.
-
 public class Ch1CollegeManagementSystem {
+
+    public static void main(String[] args) {
+        Student student1 = new Student("Deiana", "Stoianovici", "Female", "1997-24-07", new Address("Romania" , "T'imisoara", "Arhitect Ion Mincu", "16"), "26", "321232343243");
+
+        Professor professor1 = new Professor("Dan", "Popescu", "Male", "1985-03-02", new Address("Italy", "Rome","Pistachio", "1"), "39", "3213214324"));
+        Professor professor2 = new Professor("Ion", "Dan", "Male", "1984-06-02", new Address("Romania", "Timisoara","Beans", "41"), "40", "3543650913"));
+        Professor professor3 = new Professor("Maria", "Stanciu", "Female", "1990-12-06", new Address("Romania", "Bucharest","Flowers", "12"), "34", "3552115878924"));
+        Professor professor4 = new Professor("Ioana", "Dragos", "Female", "1982-03-02", new Address("Bulgaria", "Budapest","Broccoli", "24"), "42", "8970435325579"));
+        Professor professor5 = new Professor("Daniel", "Teu", "Male", "1974-02-02", new Address("Romania", "Timisoara","Pomilor", "10"), "50", "326657653654"));
+        Professor professor6 = new Professor("Alina", "Niculae", "Female", "1973-03-03", new Address("Romania", "Cluj-Napoca","Brazilor", "15"), "51", "3245467789"));
+        Professor professor7 = new Professor("Alin", "Mutascu", "Male", "1992-03-02", new Address("Romania", "Bucharest","Oglinzilor", "1"), "32", "5454657687004"));
+
+//Math, Computer Science, Physics, Chemistry, Biology, Geography, English, and History
+        Course math = new Course("Math", "Monday 08am", "60 minutes", "Learning about Math", professor2);
+        Course computerScience = new Course("Computer Science", "Monday 10am", "60 minutes", "Learning about programming", professor1);
+        Course physics = new Course("Physics", "Tuesday 09am", "60 minutes", "Learning about Physics", professor4);
+        Course chemistry = new Course("Chemistry", "Wednesday 09am", "60 minutes", "Learning about Chemistry", professor3);
+        Course biology = new Course("Biology", "Wednesday 11am", "60 minutes", "Learning about Biology", professor5);
+        Course geography = new Course("Geography", "Thursday 09am", "60 minutes", "Learning about Geography", professor6);
+        Course english = new Course("English", "Thursday 12pm", "60 minutes", "Learning about English", professor2);
+        Course history = new Course("History", "Friday 10am", "60 minutes", "Learning about History", professor7);
+
+
+    }
 
 
 
