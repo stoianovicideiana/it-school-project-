42828d87ec3c7028241fe27d240751f677408b8a
stoianovicideiana <deiana.stoianovici@gmail.com>
2024-07-29 18:42:29 +0300

    Session 16

---
+++ src/main/java/session_16_collections_framework/practice/SetPractice.java
@@ -0,0 +1,37 @@
+package session_16_collections_framework.practice;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetPractice {
+
+    public static void main(String[] args) {
+        Set<String> products = initializeSet();
+        System.out.println(products);
+
+        displayProducts(products);
+
+        Set<String> products2 = new HashSet<>();
+        products2.add("P500");
+        products2.add("P501");
+    }
+
+    public static Set<String> mergeProducts(Set<String> firstSet, Set<String> secondSet) {
+        Set<String> result = new HashSet<>(firstSet);
+        result.addAll(secondSet);
+    }
+
+    public static void displayProducts(Set<String> products) {
+        for (String product : products) {
+            System.out.println(product);
+        }
+    }
+    public static Set<String> initializeSet() {
+        Set<String> products = new HashSet<>();
+        products.add("P100");
+        products.add("P101");
+        products.add("P102");
+
+        return products;
+    }
+}
